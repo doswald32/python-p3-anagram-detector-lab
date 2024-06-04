f2779984c4568e913b1c7c286ae958e1b72b0aad
doswald32 <dan.oswald32@gmail.com>
2024-06-04 14:30:05 -0600

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
-# your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        new_list = []
+        sorted_word = sorted(self.word.lower())
+        for item in list:
+            sorted_item = sorted(item.lower())
+            if sorted_item == sorted_word and item.lower() != self.word.lower():
+                new_list.append(item)
+        return new_list
